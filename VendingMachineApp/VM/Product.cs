6e5004992f2f43dc32ab44d35b4532d3233a4d19
LiudmilaSafonova <lyusia200603@gmail.com>
2025-09-17 19:39:09 +0300

    Products: fields, dispense

---
+++ VendingMachineApp/VM/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineApp.VendingMachineApp.VM
+{
+    internal class Product
+    {
+    }
+}
