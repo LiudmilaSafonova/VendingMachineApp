e02cb175aedea24b1a76f93345321ad35b81f0f6
LiudmilaSafonova <lyusia200603@gmail.com>
2025-09-17 19:55:13 +0300

    add Product

--- VendingMachineApp/VM/Product.cs
+++ VendingMachineApp/VM/Product.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace VendingMachineApp.VendingMachineApp.VM
 {
-    internal class Product
+    public class Product
     {
+        private string Name { get; set; }
+        private decimal Price { get; set; }
+        private int Amount { get; set; }
+        private string ProductCode { get; set; }
+        public Product(string name, decimal price, int amount, string code)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+            ProductCode = code;
+        }
+
+        public string GetName() => Name;
+        public decimal GetPrice() => Price;
+        public int GetAmount() => Amount;
+        public string GetProductCode() => ProductCode;
+        public bool Dispense()
+        {
+            if (Amount > 0)
+            {
+                Amount--;
+                Console.WriteLine("Product issued.");
+                return true;
+            }
+            Console.WriteLine("Quatity of product is 0.");
+            return false;
+        }
+        public void AddProduct (int amount) => Amount += amount;
     }
 }
