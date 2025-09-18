f9db0826d1a6ded0067df4cce5f5f2a571e5966b
LiudmilaSafonova <lyusia200603@gmail.com>
2025-09-18 17:45:04 +0300

    added button 101 and changed 102

--- VendingMachineApp/VM/Program.cs
+++ VendingMachineApp/VM/Program.cs
@@ -11,8 +11,8 @@ class Program
             if (vm.GetMode() == Mode.User)
             {
                 Console.WriteLine("\n1 - Show products");
-                Console.WriteLine("2 - Insert coins");
-                Console.WriteLine("3 - Buy product [ask about coins]");
+                Console.WriteLine("2 - Insert coins[add paper moneyy]");
+                Console.WriteLine("3 - Buy product [wait for answer]");
                 Console.WriteLine("4 - Take money back [not ready]");
                 Console.WriteLine("5 - Enter admin mode");
                 Console.WriteLine("q - Quit");
@@ -20,8 +20,8 @@ class Program
             }
             else
             {
-                Console.WriteLine("101 - quantity of product [not ready]");
-                Console.WriteLine("102 - add new product [not ready, more beautiful and check for unique code]");
+                Console.WriteLine("101 - add quantity of product");
+                Console.WriteLine("102 - add new product [not ready, check for unique code]");
                 Console.WriteLine("103 - take money [not ready]");
                 Console.WriteLine("l - log out");
                 Console.WriteLine("q - Quit");
@@ -46,18 +46,30 @@ class Program
                     vm.Buy(code);
                     break;
                 case "4":
+                    Console.Write("your monet bak");
+                    break;
+                case "5":
                     Console.Write("Enter admin code: ");
                     string key = Console.ReadLine();
                     vm.SetMode(key);
                     break;
                 case "q":
                     return;
+                case "101":
+                    Console.Write("Enter produt code: ");
+                    string prod_code = Console.ReadLine();
+                    vm.FindProduct(prod_code);
+                    vm.AddAmountProduct(prod_code);
+                    break;
                 case "102":
-                    Console.WriteLine("Add new product");
+                    Console.Write("Add new product");
+                    Console.Write("Code: ");
                     string p_code = Console.ReadLine();     //TODO: check if the code isn't used
-                    
+                    Console.Write("Name: ");
                     string p_name = Console.ReadLine();
+                    Console.Write("Cost: ");
                     decimal p_cost = decimal.Parse(Console.ReadLine());
+                    Console.Write("Amount: ");
                     int p_amount = int.Parse(Console.ReadLine());
                     vm.AddProduct(p_code, p_name, p_cost, p_amount);
                     break;
